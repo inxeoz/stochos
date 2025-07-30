5f51f20af83cfb697e9065d374f134a2e38f6e6b
Konstantinos Artopoulos <artopouloskon@gmail.com>
2025-07-31 00:38:39 +0300

    feat: use client-toolkit to get screen sizes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use smithay_client_toolkit::{
+    globals::GlobalData,
+    output::{self, OutputData},
+};
 use wayland_client::{
     globals::{registry_queue_init, GlobalListContents},
     protocol::{wl_output, wl_pointer::ButtonState, wl_registry},
     Connection, Dispatch, QueueHandle,
 };
+use wayland_protocols::xdg::xdg_output::zv1::client::{zxdg_output_manager_v1, zxdg_output_v1};
 use wayland_protocols_wlr::virtual_pointer::v1::client::{
     zwlr_virtual_pointer_manager_v1, zwlr_virtual_pointer_v1,
 };
@@ -47,18 +52,51 @@ impl Dispatch<zwlr_virtual_pointer_v1::ZwlrVirtualPointerV1, ()> for State {
     }
 }
 
-impl Dispatch<wl_output::WlOutput, ()> for State {
+impl Dispatch<wl_output::WlOutput, OutputData> for State {
     fn event(
         _state: &mut Self,
         _: &wl_output::WlOutput,
         event: <wl_output::WlOutput as wayland_client::Proxy>::Event,
-        _: &(),
+        _: &OutputData,
         _: &Connection,
         _: &QueueHandle<Self>,
     ) {
         if let wl_output::Event::Mode { width, height, .. } = event {
             println!("Found output mode: {}x{}", width, height);
         }
+    }
+}
+
+impl Dispatch<zxdg_output_v1::ZxdgOutputV1, OutputData> for State {
+    fn event(
+        _state: &mut Self,
+        _: &zxdg_output_v1::ZxdgOutputV1,
+        event: zxdg_output_v1::Event,
+        _: &OutputData,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        match event {
+            zxdg_output_v1::Event::LogicalSize { width, height } => {
+                println!("Found logical size: {}x{}", width, height);
+            }
+            zxdg_output_v1::Event::Name { name } => {
+                println!("Found name: {}", name);
+            }
+            _ => {}
+        };
+    }
+}
+
+impl Dispatch<zxdg_output_manager_v1::ZxdgOutputManagerV1, GlobalData> for State {
+    fn event(
+        _state: &mut Self,
+        _proxy: &zxdg_output_manager_v1::ZxdgOutputManagerV1,
+        _event: zxdg_output_manager_v1::Event,
+        _data: &GlobalData,
+        _conn: &Connection,
+        _qhandle: &QueueHandle<Self>,
+    ) {
     }
 }
 
@@ -69,7 +107,9 @@ fn main() -> anyhow::Result<()> {
     let (globals, mut event_queue) = registry_queue_init::<State>(&conn)?;
     let qh = event_queue.handle();
 
-    globals.bind::<wl_output::WlOutput, _, _>(&qh, 1..=2, ())?;
+    let _ = output::OutputState::new(&globals, &qh);
+
+    // globals.bind::<wl_output::WlOutput, _, _>(&qh, 1..=2, ())?;
 
     let manager: zwlr_virtual_pointer_manager_v1::ZwlrVirtualPointerManagerV1 =
         globals.bind(&qh, 1..=2, ())?;
